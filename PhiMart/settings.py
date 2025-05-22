0ea70ef5d00d5b03a545fed5cdd96cd743caed81
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2025-05-22 19:46:05 +0600

    fixtures

--- PhiMart/settings.py
+++ PhiMart/settings.py
@@ -43,9 +43,11 @@ INSTALLED_APPS = [
     'product',
     'order',
     'users',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -73,6 +75,12 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'PhiMart.wsgi.application'
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
 
 
 # Database
