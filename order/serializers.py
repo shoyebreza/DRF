252fcf2bfa0da6e845cdc8448b561c6b5ae4bb65
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2025-05-30 22:55:21 +0600

    cart total price

--- order/serializers.py
+++ order/serializers.py
@@ -1,19 +1,23 @@
 from rest_framework import serializers
 from order.models import Cart, CartItem
 from product.serializers import ProductSerializer
+from product.models import Product
 
-
-
+class SimpleProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id','name','price'] 
 
 class CartItemSerializer(serializers.ModelSerializer):
-    product = ProductSerializer()
-    product_price = serializers.SerializerMethodField(method_name='get_product_price')
+    product = SimpleProductSerializer()
+    total_price = serializers.SerializerMethodField(method_name='get_total_price')
+
     class Meta:
         model = CartItem
-        fields = ['id','product','quantity','product_price']
+        fields = ['id','product','quantity','product','total_price']
 
-    def get_product_price(self,cart_item):
-        return cart_item.product.price
+    def get_total_price(self,cart_item: CartItem):
+        return cart_item.quantity * cart_item.product.price
 
 
 
@@ -21,9 +25,14 @@ class CartItemSerializer(serializers.ModelSerializer):
 
 class CartSerializer(serializers.ModelSerializer):
     items = CartItemSerializer(many=True)
+    total_price = serializers.SerializerMethodField(method_name='get_total_price')
     class Meta:
         model = Cart
-        fields = ['id','user','items']
+        fields = ['id','user','items','total_price']
+
+    def get_total_price(self, cart: Cart):
+        list = sum([item.product.price * item.quantity for item in cart.items.all()])
+        return list
 
 
 
