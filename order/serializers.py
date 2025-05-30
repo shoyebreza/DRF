e5967a375384a007fb6ff180b9f631c1b1b02ea7
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2025-05-30 21:24:01 +0600

    cart custom method

--- order/serializers.py
+++ order/serializers.py
@@ -1,9 +1,31 @@
 from rest_framework import serializers
 from order.models import Cart, CartItem
+from product.serializers import ProductSerializer
+
+
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    product = ProductSerializer()
+    product_price = serializers.SerializerMethodField(method_name='get_product_price')
+    class Meta:
+        model = CartItem
+        fields = ['id','product','quantity','product_price']
+
+    def get_product_price(self,cart_item):
+        return cart_item.product.price
+
+
+
 
 
 class CartSerializer(serializers.ModelSerializer):
+    items = CartItemSerializer(many=True)
     class Meta:
         model = Cart
-        fields = ['id','user']
+        fields = ['id','user','items']
+
+
+
+
 
