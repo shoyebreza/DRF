083519250d7d6a6749850a07f781ffe573a4b28b
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2025-06-09 19:41:36 +0600

    transaction

--- order/serializers.py
+++ order/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from order.models import Cart, CartItem, Order,OrderItem
 from product.serializers import ProductSerializer
 from product.models import Product
+from order.services import OrderService
 
 class SimpleProductSerializer(serializers.ModelSerializer):
     class Meta:
@@ -80,26 +81,11 @@ class CreateOrderSerializer(serializers.Serializer):
         user_id = self.context['user_id']
         cart_id = validated_data['cart_id']
 
-        cart = Cart.objects.get(pk=cart_id)
-        cart_items = cart.items.select_related('product').all()
-        total_price = sum([item.product.price * item.quantity for item in cart_items])
-        order = Order.objects.create(user_id = user_id, total_price = total_price)
-        order_items = [
-            OrderItem(
-                order=order, 
-                product=item.product,
-                price = item.product.price,
-                quantity = item.quantity,
-                total_price = item.product.price * item.quantity
-            )
-            for item in cart_items
-        ]
-
-        OrderItem.objects.bulk_create(order_items)
-
-        cart.delete()
-        return order
-    
+        try:
+            order = OrderService.create_order(user_id=user_id, cart_id=cart_id)
+            return order
+        except ValueError as e:
+            raise serializers.ValidationError(str(e))
 
     def to_representation(self, instance):
         return OrderSerializer(instance).data
