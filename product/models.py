5d6320f77f0938c8bd8b2a7f287f14dfcd360705
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2025-06-11 12:38:51 +0600

    image validator

--- product/models.py
+++ product/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.core.validators import MinValueValidator, MaxValueValidator
+from product.validators import validate_file_size
 
 # Create your models here.
 
@@ -33,8 +34,7 @@ class Product(models.Model):
 class ProductImage(models.Model):
     product = models.ForeignKey(
         Product, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField(
-        upload_to="products/images/")
+    image = models.ImageField(upload_to="products/images/", validators=[validate_file_size])
     
 
 
